1163e91836731c1af04f2e70f1a1bb574540013e
Vitalik Lavrenko <vitaliklavrenko@MacBook-Pro-Vitalik.local>
2018-02-06 03:20:50 +0200

    fix bugs

--- src/components/formComponent/formComponent.js
+++ src/components/formComponent/formComponent.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import Paper from 'material-ui/Paper';
- import TextField from 'material-ui/TextField';
-import Phone from 'react-phone-number-input'
-import rrui from 'react-phone-number-input/rrui.css'
-import rpni from 'react-phone-number-input/style.css'
+import RaisedButton from 'material-ui/RaisedButton';
+import validator from 'validator';
+import Form from 'react-validation/build/form';
+import Input from 'react-validation/build/input';
 import '../../style/style.css'
 
 
@@ -11,7 +11,7 @@ export default class Test extends Component {
   constructor (props) {
     super(props)
     this.state = {
-
+     isValid: true,
      value: ''
    }
 
@@ -21,13 +21,18 @@ export default class Test extends Component {
     this.changeAddress = this.changeAddress.bind(this)
     this.changePostcode = this.changePostcode.bind(this)
     this.changeDate = this.changeDate.bind(this)
+    this.save = this.save.bind(this)
   }
 
-  componentDidMount () {
-    console.log('props in component', this.props)
-  }
 
-  changeName ({target: {value}}) {
+  changeName ({target: {value, name}}) {
+    let errors = {}
+    if (name === 'someUserName' && (!value || value.length === 0)) {
+    errors['nameError'] = 'This field should not be blank.'
+    this.setState({
+        isValid: false
+    })
+}
     this.props.changeStateProps('name', value)
   }
 
@@ -36,82 +41,119 @@ export default class Test extends Component {
 
   }
 
- changePhone ({target: {value}}) {
-  this.props.changeStateProps('phone', value)
-}
+  changePhone ({target: {value}}) {
+    this.props.changeStateProps('phone', value)
+  }
 
-changeAddress ({target: {value}}) {
- this.props.changeStateProps('address', value)
-}
+  changeAddress ({target: {value}}) {
+    this.props.changeStateProps('address', value)
+  }
 
-changePostcode ({target: {value}}) {
- this.props.changeStateProps('postcode', value)
-}
+  changePostcode ({target: {value}}) {
+    this.props.changeStateProps('postcode', value)
+  }
 
-changeDate ({target: {value}}) {
-this.props.changeStateProps('date', value)
-}
+  changeDate ({target: {value}}) {
+    this.props.changeStateProps('date', value)
+  }
+
+  save() {
+
+    if (!this.state.isValid) {
+        return
+    }
+
+    if (this.state.isValid) {
+        window.location.reload();
+        this.setState({
+            user: {
+                someUserName: '',
+                someUserEmail: '',
+                someUserPhone: '',
+                someUserAddress: '',
+                someUserPostcode: '',
+                dateOfBirth: null,
+            }
+        })
+    }
+  }
 
   render () {
-        const style = {
-        height: 310,
-        width: 350,
-        margin: 20,
-        marginLeft: 140,
-        display: 'inline-block',
+
+      const required = (value) => {
+       if (!value.toString().trim().length) {
+         return <span className="error"> require* </span>
+       }
+     }
+
+      const email = (value) => {
+       if (!validator.isEmail(value)) {
+         return <span className="error"> {value} is not a valid email.</span>
+       }
       }
+
     return (
      <div className="fields" >
-       <Paper style={style} zDepth={5} rounded={false} >
+       <Paper className="paper" zDepth={5} rounded={false} >
+         <Form>
+            <Input
+              className="name"
+              name="name"
+              type="text"
+              placeholder="Name"
+              onChange={this.changeName}
+              validations={[required]}
+            />
 
-          <TextField
-            className="name"
-            name="name"
-            type="text"
-            placeholder="Name"
-            onChange={this.changeName}
+            <Input
+              className="email"
+              name="email"
+              type="email"
+              placeholder="Email"
+              onChange={this.changeEmail}
+              validations={[required, email]}
+            />
 
-          />
+            <Input
+              className='phone'
+              maxLength="16"
+              placeholder="Enter phone number"
+              type="tel"
+              onChange={ this.changePhone }
+            />
 
-          <TextField
-            className="email"
-            name="email"
-            type="email"
-            placeholder="Email"
-            onChange={this.changeEmail}
-          />
+            <Input
+              className="address"
+              type="text"
+              placeholder="Address"
+              onChange={this.changeAddress}
+            />
 
-          <TextField
-            className='phone'
-            maxLength="16"
-            placeholder="Enter phone number"
-            type="tel"
-            onChange={ this.changePhone }
-          />
+            <Input
+              className="postcode"
+              name="postcode"
+              type="text"
+              placeholder="Postcode"
+              onChange={this.changePostcode}
+              validations={[required]}
+            />
 
-          <TextField
-            className="address"
-            type="text"
-            placeholder="Address"
-            onChange={this.changeAddress}
-          />
+            <Input
+              className="date"
+              name="calendar"
+              type="date"
+              placeholder="Date of birth"
+              onChange={this.changeDate}
+              validations={[required]}
+            />
 
-          <TextField
-            className="postcode"
-            name="postcode"
-            type="number"
-            placeholder="Postcode"
-            onChange={this.changePostcode}
-          />
-
-          <TextField
-            className="date"
-            name="calendar"
-            type="date"
-            placeholder="Date of birth"
-            onChange={this.changeDate}
-          />
-
+            <RaisedButton
+              className="btn"
+              label="Save"
+              primary={true}
+              onClick={this.save}>
+            </RaisedButton>
+          </Form>
         </Paper>
       </div>
     )
